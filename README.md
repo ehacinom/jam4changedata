f15e880f107c728af00ab25f661dc8aee8ac9096
ehacinom <ehacinom@gmail.com>
2016-12-17 13:07:55 -0700

    committee metadata done

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 #startup
 
-Committees
-==========
+##Committees
 Notice that the committees are bienniel and oddnumbered years. We are currently in the 2015 year, but soon switching to 2017.
 
 [Wisconsin Committees](http://docs.legis.wisconsin.gov/2015/committees)
 
-For each of the links below, view source gives these links
+View source on each of those 4 committee types (Senate, Assembly, Joint, Other) to find the "feed" links
 
-*[Senate](http://docs.legis.wisconsin.gov/feed/2015/committees/senate)
-*[Assembly](http://docs.legis.wisconsin.gov/feed/2015/committees/assembly)
-*[Joint](http://docs.legis.wisconsin.gov/feed/2015/committees/joint)
-*[Other](http://docs.legis.wisconsin.gov/feed/2015/committees/other)
+* [Senate](http://docs.legis.wisconsin.gov/feed/2015/committees/senate)
+* [Assembly](http://docs.legis.wisconsin.gov/feed/2015/committees/assembly)
+* [Joint](http://docs.legis.wisconsin.gov/feed/2015/committees/joint)
+* [Other](http://docs.legis.wisconsin.gov/feed/2015/committees/other)
 
-I extract the committees from these sites using 
-    get_data.py
-output to 
+I extract the committee metadata from these sites. Note that for the Joint Committees, there's a lot of reports and extra stuff that has to be edited out. This is done with get_data.edit_JointCommittee(). In the future, every time the data is updated, check that the information skipped is reasonable.
+
+###Files
+* get_data.py / get_data.ipynb has code
+* committee_list.txt is a list of all the committee names
+* committees.csv is all committee information
+
+###In progress
+From the metadata, I link to committee sites, and extract information on each of those.
+
+I output to committees.csv
+
--- get_data.py
+++ get_data.py
@@ -1,20 +1,147 @@
 import urllib
+import xmltodict
+import csv
 
-
-
-
-
-def load_xml_data(url):
+def load_txt(url):
     """
+    Grab text from url
+    
     INPUT
-    str url
+    url of text/xml data
     
     OUTPUT
-    dict xml data
+    text/xml data
+        
+    """
+    # load txt
+    fp = urllib.urlopen(url)
+    text = fp.read()
+    fp.close()
     
-    Retrieves data through URL get
+    return text
+
+def edit_JointCommittee(item):
+    """
+    Dealing with Joint Committee reports
+    (esp from the Joint Legislative Audit Committee)
+    
+    In the future, check that the information skipped is reasonable
+    
+    INPUT
+    item, the list of OrderedDicts with metadata on each committee
+    Parameters and example data:
+        (u'guid', OrderedDict([(u'@isPermaLink', u'false'), ('#text', u'5b7a05a6-a615-4dd9-94d6-7346f51a1363')]))
+        (u'link', u'http://docs.legis.wisconsin.gov/document/committee/2015/1475')
+        (u'title', u'University of Wisconsin Hospitals and Clinics Authority - 2016-12-17')
+        (u'description', u'University of Wisconsin Hospitals and Clinics Authority')
+        (u'pubDate', u'Sat, 17 Dec 2016 07:35:58 -0600')
+        (u'a10:updated', u'2016-12-17T07:35:58-06:00')
+    
+    OUTPUT
+    boolean, if data will be used
     
     """
     
-    url = urllib.urlopen(url)
-    return 
+    OtherJointCommitteeCrap = 'records'
+    AnnoyingJointCommittees = ['Presentation', 'Report', 'Proceedings', 
+                               'Minutes', 'Proposed', 'Audio', 'Agenda']
+    
+    name = item['description']
+    title = item['title']
+
+    if not name: 
+        return False
+    
+    if OtherJointCommitteeCrap in name:
+        return False
+    
+    for ajc in AnnoyingJointCommittees:
+        if ajc in title:
+            return False
+    
+    return True
+
+def get_meta_committee(text, committee_type):
+    """
+    Get metadata for committees
+    
+    INPUT
+    text from load_txt(url)
+    
+    INTERMEDIARIES
+    x is a list of items/OrderedDicts with metadata on each committee
+    Parameters and example data:
+        (u'guid', OrderedDict([(u'@isPermaLink', u'false'), ('#text', u'5b7a05a6-a615-4dd9-94d6-7346f51a1363')]))
+        (u'link', u'http://docs.legis.wisconsin.gov/document/committee/2015/1475')
+        (u'title', u'University of Wisconsin Hospitals and Clinics Authority - 2016-12-17')
+        (u'description', u'University of Wisconsin Hospitals and Clinics Authority')
+        (u'pubDate', u'Sat, 17 Dec 2016 07:35:58 -0600')
+        (u'a10:updated', u'2016-12-17T07:35:58-06:00')
+    
+    OUTPUT
+    data, [name, committee_type, link]
+    
+    """
+    
+    # x is a list of OrderedDict
+    # should do some error catching here
+    # find format of data through testing / pretty xml
+    x = xmltodict.parse(text)['rss']['channel']['item']
+    
+    # iterate for relevant information
+    data = []
+    for item in x:
+        name = item['description']
+        
+        interest = True
+        if not name: 
+            interest = False
+        
+        # dealing with Joint Committee reports
+        if committee_type == 'Joint':
+            interest = edit_JointCommittee(item)
+        
+        # add to data
+        if interest:
+            data.append([name, committee_type, item['link']])
+    
+    return data
+
+def get_committee(out = 'committees.csv'):
+    """
+    Get all data for committees
+    
+    INPUT
+    out the output file
+    
+    SAVE TO FILE out.
+    [name, link, Y-M-D]
+    
+    SAVE TO FILE committee_list.txt
+    names of all the committees
+    
+    """
+    
+    root = 'http://docs.legis.wisconsin.gov/feed/2015/committees/'
+    committee_type = ['Senate', 'Assembly', 'Joint', 'Other']
+    
+    data = []
+    for ct in committee_type:
+        # metadata
+        text = load_txt(root+ct)
+        metadata = get_meta_committee(text, ct)
+        
+        # do some other research
+        # follow some links
+        
+        # add to data
+        data.extend(metadata)
+
+    # save list of committees to file
+    with open('committee_list.txt', 'w') as f:
+        for m in data:
+             f.write(m[0]+'\n')
+    
+#     # write
+#     with open(out, 'a') as f:
+#         f.write()
